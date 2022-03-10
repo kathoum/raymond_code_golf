8bc650a4b3327439a8b97978c8fd7e1210b3cf76
Dario Saccavino <kathoum@gmail.com>
2022-03-10 11:37:40 +0100

    Remove redundant AND

--- vad.c
+++ vad.c
@@ -17,8 +17,8 @@ void va_darken(union Pixel* first, union Pixel* last, int darkness)
   __m256i mult_ga = _mm256_set1_epi32((256 << 16) | lightness);
   for (; first < last-7; first += 8) {
     __m256i p = _mm256_loadu_si256((__m256i*)first);
-    __m256i rb = _mm256_and_si256(mask_rb, _mm256_srli_epi16(_mm256_mullo_epi16(mult_rb, _mm256_and_si256(mask_rb, p)), 8));
-    __m256i ga = _mm256_andnot_si256(mask_rb, _mm256_mullo_epi16(mult_ga, _mm256_and_si256(mask_rb, _mm256_srli_epi16(p, 8))));
+    __m256i rb = _mm256_srli_epi16(_mm256_mullo_epi16(mult_rb, _mm256_and_si256(mask_rb, p)), 8);
+    __m256i ga = _mm256_andnot_si256(mask_rb, _mm256_mullo_epi16(mult_ga, _mm256_srli_epi16(p, 8)));
     _mm256_storeu_si256((__m256i*)first, _mm256_or_si256(rb, ga));
   }
   v2_darken(first, last, darkness);
